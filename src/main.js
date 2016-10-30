6b3fc23127ddcd2b74b01bb92a6a00e41989cf1e
stk132 <stk0724@gmail.com>
2016-10-30 23:22:25 +0900

    add command line option

--- src/main.js
+++ src/main.js
@@ -2,13 +2,34 @@ require("babel-polyfill");
 const client  = require('cheerio-httpcli');
 const request = require('request');
 const fs = require('fs');
+const mkdirp = require('mkdirp');
 const axios = require('axios');
+const program = require('commander');
+program
+  .version('0.0.1')
+  .option('-d, --download-dir <n>', 'pdf download directory')
+  .option('-c, --download-count <n>', 'pdf download count')
+  .parse(process.argv)
+
+let pdf_dir = '/tmp';
+let pdf_dl_cnt = '10';
+
+if (program.downloadDir) {
+  pdf_dir = program.downloadDir;
+  if (!fs.existsSync(pdf_dir)) {
+    mkdirp.sync(pdf_dir);
+  } else if (!fs.statSync(pdf_dir).isDirectory()){
+    console.error(`error : ${pdf_dir} is not directory`);
+    process.exit(1);
+  }
+}
+if (program.downloadCount) pdf_dl_cnt = program.downloadCount;
 
 axios.post('https://getpocket.com/v3/get', {
   "consumer_key": process.env.POCKET_CONSUMER_KEY,
   "access_token": process.env.POCKET_ACCESS_TOKEN,
   "state": "unread",
-  "count": "10",
+  "count": pdf_dl_cnt,
   "sort": "newest",
   "search": "speakerdeck"
 })
@@ -19,13 +40,15 @@ axios.post('https://getpocket.com/v3/get', {
     const url_ary = speakerdeck_url.split('/')
     const title = url_ary[url_ary.length - 1];
     let result = null;
+
     try {
       console.log('access start');
       result = await client.fetch(speakerdeck_url);
       console.log('access done');
     } catch (err) {
-      console.log('error occured');
-      console.log(err);
+      console.error(`error occured at ${speakerdeck_url}`);
+      console.error(err);
+      console.log(`skip ${speakerdeck_url} due to error`);
       continue;
     }
 
@@ -35,13 +58,11 @@ axios.post('https://getpocket.com/v3/get', {
     const pdf_title = result.$('title').text().split(' ')[0];
     console.log(`download ${pdf_title}`);
     request.get(pdf_link)
-    .on('response', (res) => {
-      console.log(res);
-    })
-    .pipe(fs.createWriteStream(`${process.env.HOME}/speakerdeck_pdf/${title}.pdf`));
+    .pipe(fs.createWriteStream(`${pdf_dir}/${title}.pdf`));
   }
 
 })
 .catch((error) => {
-  console.log(error);
+  console.error(error);
+  process.exit(1);
 });
